19f00b8884bd92fba18675ef318c8fd0324484f2
Ceasar2001 <ecleoceasar1@gmail.com>
2025-11-18 12:52:12 +0800

    fixing bug

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+import dotenv from 'dotenv';
+dotenv.config(); // load .env first
+
 import express from 'express';
 import cors from 'cors';
 import connectDB from './db/connection.js';
@@ -8,11 +11,10 @@ app.use(cors());
 app.use(express.json());
 app.use('/api/auth', authRoutes);
 
-
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-    connectDB();
+app.listen(PORT, async () => {
+    console.log("MONGO_URI =", process.env.MONGO_URI); // debug
+    await connectDB();
     console.log(`Server is running on port ${PORT}`);
 });
-
